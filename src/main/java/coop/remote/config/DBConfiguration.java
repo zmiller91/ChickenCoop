ae39ea6e8282638cc7cd8a6c9f92b6920ba7df3c
zmiller91 <me@zackmiller.info>
2025-02-01 00:10:29 -0600

    Just work please

--- src/main/java/coop/remote/config/DBConfiguration.java
+++ src/main/java/coop/remote/config/DBConfiguration.java
@@ -19,6 +19,7 @@ import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.net.URLEncoder;
 
 @Log4j2
 @Configuration
@@ -42,13 +43,18 @@ public class DBConfiguration {
 
             JsonObject info = JsonParser.parseString(response.getSecretString()).getAsJsonObject();
 
-            log.info("ZZZ Username: " + info.get("username").getAsString());
-            log.info("ZZZ Password: " + info.get("password").getAsString());
-            log.info("ZZZ URL: " + "jdbc:mysql://" + info.get("host").getAsString() + "/local_pi");
+            String u = "ChickenCoopProd";
+            String p = "aZlGRCDQMTVS4XVccg7_Cnr8,HwIM5";
+            String h = "codedeploypipelinestack-mysqlinstance1ff16bc1-wmtp4t0ixz3p.c78iqmoasiqg.us-east-1.rds.amazonaws.com";
+
+            log.info("ZZZ Username: " + u);
+            log.info("ZZZ Password: " + p);
+            log.info("ZZZ URL: " + "jdbc:mysql://" + h + "/local_pi");
 
             builder.username(info.get("username").getAsString())
                     .password(info.get("password").getAsString())
-                    .url("jdbc:mysql://" + info.get("host").getAsString() + "/local_pi?ssl=true");
+//                    .url("jdbc:mysql://" + info.get("host").getAsString() + ":3306/local_pi?ssl=true")
+                    .url("jdbc:mysql://" + h + ":3306/useSSSL=false&local_pi?user=" + u + "&password=" + p);
 
         } else {
             builder.username(username)
