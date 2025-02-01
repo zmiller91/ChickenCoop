8e028c089289845b3a3afeb971a888a99bd9c49e
zmiller91 <me@zackmiller.info>
2025-01-31 21:31:48 -0600

    Fixing shutdown script

--- scripts/aws/shutdown.sh
+++ scripts/aws/shutdown.sh
@@ -1,2 +1,6 @@
 cd /home/ec2-user/coop
-ps -aux | grep ChickenCoop-1.0-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | xargs sudo kill -9
+
+
+ps -aux | grep ChickenCoop-1.0-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | if [ -s /dev/stdin ]; then
+  xargs sudo kill -9
+fi
