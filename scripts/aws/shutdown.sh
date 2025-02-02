422ea5928feb4e8800fdf89052569dd6deb80943
zmiller91 <me@zackmiller.info>
2025-02-01 22:19:46 -0600

    Trying to fix some scripts

--- scripts/aws/shutdown.sh
+++ scripts/aws/shutdown.sh
@@ -1,6 +1,5 @@
 cd /home/ec2-user/coop
 
-
-ps -aux | grep ChickenCoop-1.0-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | if [ -s /dev/stdin ]; then
-  xargs sudo kill -9
+if [ $(ps -aux | grep ChickenCoop-1.0-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | wc -l) -ne 0 ]; then
+    ps -aux | grep ChickenCoop-1.0-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | xargs sudo kill -9
 fi
--- scripts/aws/start.sh
+++ scripts/aws/start.sh
@@ -1,2 +1,2 @@
 cd /home/ec2-user/coop
-nohup sudo java -jar ChickenCoop-1.0-SNAPSHOT.jar --spring.profiles.active=aws &
+nohup sudo java -jar ChickenCoop-1.0-SNAPSHOT.jar --spring.profiles.active=aws & > /dev/null
