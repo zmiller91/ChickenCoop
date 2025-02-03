84bcb79ca3caed3b42eabd285ec39ff8c57854de
zmiller91 <me@zackmiller.info>
2025-02-02 22:29:56 -0600

    Updating mysql secret key

--- scripts/aws/mysqlconnect.sh
+++ scripts/aws/mysqlconnect.sh
@@ -1,4 +1,4 @@
-SECRET_STRING=$(aws secretsmanager get-secret-value --secret-id MysqlInstanceSecretC144D3B7-2LUy4Xg4043M --query SecretString --output text)
+SECRET_STRING=$(aws secretsmanager get-secret-value --secret-id MysqlInstanceSecretC144D3B7-aU4JVlDRkqeW --query SecretString --output text)
 HOST=$(echo $SECRET_STRING | jq -r .host)
 USERNAME=$(echo $SECRET_STRING | jq -r .username)
 PASSWORD=$(echo $SECRET_STRING | jq -r .password)
