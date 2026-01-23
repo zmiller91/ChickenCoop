8586933d81d36790c1659fc150332f95e098fc20
zmiller91 <me@zackmiller.info>
2026-01-22 23:35:18 -0600

    Fixing bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,5 @@
-DB_USER=$(grep '^db_user=' context.txt | cut -d= -f2-)
-DB_PASSWORD=$(grep '^db_password=' context.txt | cut -d= -f2-)
+DB_USER=$(grep '^db_user=' ~/context.txt | cut -d= -f2-)
+DB_PASSWORD=$(grep '^db_password=' ~/context.txt | cut -d= -f2-)
 
 # Create local web database if it doesnt exist
 mysql -u "$DB_USER" -p"$DB_PASSWORD" <<EOF
