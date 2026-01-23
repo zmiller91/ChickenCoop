d4d2fc2fc45b2b0c7dfb91f628a87ec62f6509c0
zmiller91 <me@zackmiller.info>
2026-01-23 10:58:23 -0600

    Fixing pipeline

--- scripts/aws/createtables.sh
+++ scripts/aws/createtables.sh
@@ -4,4 +4,4 @@ USERNAME=$(echo $SECRET_STRING | jq -r .username)
 PASSWORD=$(echo $SECRET_STRING | jq -r .password)
 
 cd /home/ec2-user/coop
-mysql -u $USERNAME -p"${PASSWORD}" -h "${HOST}" < tables.sql
+mysql -u $USERNAME -p"${PASSWORD}" -h "${HOST}" < sql/shared/V1__init_web_tables.sql
