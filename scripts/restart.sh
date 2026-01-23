303b6f14037811e30ce6f76688077a7ec0fad501
zmiller91 <me@zackmiller.info>
2026-01-22 23:30:36 -0600

    Fixing restart script

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,13 +1,13 @@
 echo "Executing startup script"
 
-
-cd /home/pi/projects/ChickenCoop/target
-
 echo "Stopping any running processes"
 ps -aux | grep ChickenCoop-1.0-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | xargs sudo kill -9
 
+cd ~/projects/ChickenCoop/scripts
 echo "Bootstrapping"
 ./bootstrap.sh
+
+cd /home/pi/projects/ChickenCoop/target
 
 echo "Starting process"
 nohup sudo java -jar ChickenCoop-1.0-SNAPSHOT.jar --spring.profiles.active=pi >> /home/pi/pi-build/log.txt &
