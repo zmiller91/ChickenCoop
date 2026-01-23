e78449f9921a657ca2a95e4393facb267b642b5e
zmiller91 <me@zackmiller.info>
2026-01-22 23:46:53 -0600

    trying to fix this bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,12 +1,15 @@
-DB_USER=$(grep '^db_user=' ~/context.txt | cut -d= -f2-)
-DB_PASSWORD=$(grep '^db_password=' ~/context.txt | cut -d= -f2-)
+DB_USER=$(grep '^db_user=' ~/pi_conf/context.txt | cut -d= -f2-)
+DB_PASSWORD=$(grep '^db_password=' ~/pi_conf/context.txt | cut -d= -f2-)
 
-# Create local web database if it doesnt exist
-mysql -u "$DB_USER" -p"$DB_PASSWORD" <<EOF
+sudo mysql <<EOF
 CREATE DATABASE IF NOT EXISTS local_pi;
-EOF
-
-# Create pi state database if it doesnt exist
-mysql -u "$DB_USER" -p"$DB_PASSWORD" <<EOF
 CREATE DATABASE IF NOT EXISTS pi_state;
+
+CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost'
+IDENTIFIED BY '${DB_PASSWORD}';
+
+GRANT ALL PRIVILEGES ON local_pi.* TO '${DB_USER}'@'localhost';
+GRANT ALL PRIVILEGES ON pi_state.* TO '${DB_USER}'@'localhost';
+
+FLUSH PRIVILEGES;
 EOF
