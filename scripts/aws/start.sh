9430a1b9da7af9c052d6740820962002d4f9dd2b
zmiller91 <me@zackmiller.info>
2026-01-23 13:58:14 -0600

    fixing issue with main class in pom

--- scripts/aws/start.sh
+++ scripts/aws/start.sh
@@ -1,2 +1,2 @@
 cd /home/ec2-user/coop
-nohup sudo java -jar target/ChickenCoop-1.0-SNAPSHOT.jar --spring.profiles.active=aws > /dev/null 2> /dev/null < /dev/null &
+nohup sudo java -jar ChickenCoop-1.0-SNAPSHOT.jar --spring.profiles.active=aws > /dev/null 2> /dev/null < /dev/null &
